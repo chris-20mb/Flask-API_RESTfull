af58b03bed2b67f05284e20f386a670953f5562b
Christopher <christophermachado222@gmail.com>
2024-10-08 16:53:29 -0300

    "app factory"

--- src/app.py
+++ src/app.py
@@ -1,39 +1,28 @@
-from flask import Flask, url_for, request, jsonify
+import os
 
-app = Flask(__name__)
+from flask import Flask
 
-@app.route("/olamundo/<usuario>/<int:idade>/<float:altura>")
-def hello_world(usuario, idade, altura):
-    return jsonify(
-        {
-            "Usuário": usuario,
-            "Idade": idade,
-            "Altura": altura
-        }
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'diobank.sqlite'),
     )
-    
 
-
-@app.route("/bemvindo")
-def bem_vindo():
-    return "<h1>Bem, vindo!</h1>"
-
-@app.route("/projects/")
-def projects():
-    return "The project page"
-
-@app.route("/about", methods=["GET","POST"])
-def about():
-    if request.method == 'GET':
-        return 'This is a GET'
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
     else:
-        return 'This is a POST'
-    return "The about page"
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
 
-with app.test_request_context():
-    print(url_for('bem_vindo'))
-    print(url_for('projects'))
-    print(url_for('about', next='/'))
-    print(url_for('hello_world', usuario='Christopher', idade=23, altura=1.84))
-   
+    return app
