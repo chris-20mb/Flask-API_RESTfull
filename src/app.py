92038edded27ccab3f6f24414461a1c86ec340ca
Christopher <christophermachado222@gmail.com>
2024-10-10 12:06:49 -0300

    "criando classes para mapeamento de dados"

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
 import os
 
 import click
+import sqlalchemy as sa
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
  
 
 
@@ -12,6 +16,26 @@ class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(model_class=Base)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+   
+    def __repr__(self) -> str:
+         return f"User(id={self.id!r}, username={self.name!r})"
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True) 
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey('user.id'))
+
+
+    def __repr__(self) -> str:
+         return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+
 
 @click.command('init-db')
 def init_db_command():
